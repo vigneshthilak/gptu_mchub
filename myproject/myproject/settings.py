52052dadcb495223d18e893fb332ac01a3463b87
vigneshthilak <vigneshthilagaraj00@gmail.com>
2025-01-29 06:39:43 +0530

    The commited was maded by Vignesh

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gptumchub',
+        'USER': 'usergptu',
+        'PASSWORD': 'gptu139',
+        'HOST': 'localhost',  # Or your DB host
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
