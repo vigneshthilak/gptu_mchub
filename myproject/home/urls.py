079a672c9adaba64f20e77f11e15ad4ae2827a46
vigneshthilak <vigneshthilagaraj00@gmail.com>
2025-02-09 23:56:37 +0530

    The DB is connected successfully with the login.html

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("login/", views.login, name='login'),
+    path("thanks/", views.thanks, name='thanks'),
+    path("sorry/", views.sorry, name='sorry'),
 ]
